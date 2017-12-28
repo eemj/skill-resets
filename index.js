890899986b7db3e97e5e6b4480a2d8717026fa7e
eemj <jamiefarrugia@msn.com>
2017-12-28 13:49:18 +0100

    Update S_LOGIN and clean code

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-const SHOW_SYSTEM_RESET_MESSAGE = false;
-const RESET_FONT_COLOR = '#FF4500'; // https://www.google.com/search?q=colour+picker
-const FLASHING_NOTIFICATION = false;
+const SHOW_SYSTEM_RESET_MESSAGE = false
+const RESET_FONT_COLOR = '#FF4500' // https://www.google.com/search?q=colour+picker
+const FLASHING_NOTIFICATION = false
 
-module.exports = function SkillResets(dispatch) {
-	let model;
+module.exports = function SkillResets (dispatch) {
+  let model = null
 
-	dispatch.hook('S_LOGIN', 1, event => ({ model } = event));
+  dispatch.hook('S_LOGIN', 9, event => {
+    ({ templateId: model } = event)
+  })
 
-	const showMessage = message => {
-		dispatch.toClient('S_DUNGEON_EVENT_MESSAGE', 1, {
-			message,
-			unk1: FLASHING_NOTIFICATION ? 70 : 2,
-			unk2: 0,
-			unk3: 0
-		});
-	};
+  const showMessage = message => {
+    dispatch.toClient('S_DUNGEON_EVENT_MESSAGE', 1, {
+      message,
+      unk1: FLASHING_NOTIFICATION ? 70 : 2,
+      unk2: 0,
+      unk3: 0
+    })
+  }
 
-	dispatch.hook('S_CREST_MESSAGE', 1, event => {
-		if (event.type === 6) {
-			showMessage(`<img src="img://skill__0__${model}__${event.skillID}" width="48" height="48" vspace="-20"/><font size="24" color="${RESET_FONT_COLOR}">&nbsp;Reset</font>`);
-			if (!SHOW_SYSTEM_RESET_MESSAGE) return false;
-		}
-	});
-};
+  dispatch.hook('S_CREST_MESSAGE', 1, ({ type, skillID }) => {
+    if (type === 6) {
+      showMessage(
+        `<img src="img://skill__0__${model}__${
+          skillID
+        }" width="48" height="48" vspace="-20"/><font size="24" color="${
+          RESET_FONT_COLOR
+        }">&nbsp;Reset</font>`
+      )
+      if (!SHOW_SYSTEM_RESET_MESSAGE) return false
+    }
+  })
+}
